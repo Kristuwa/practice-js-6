a4d137822f9d5cd0e43f1a1d1f56132dc7d65c96
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-05-08 13:17:47 +0300

    Update script6.js

--- js/script6.js
+++ js/script6.js
@@ -1952,39 +1952,55 @@ function check(str, bracketsConfig) {
   }
 
   const brackets = bracketsConfig.flat();
-  let open = [];
-  let close = [];
+  console.log(brackets);
 
-  for (let i = 0; i < brackets.length; i += 1) {
-    if (i % 2 === 0) {
-      open.push(brackets[i]);
-    } else {
-      close.push(brackets[i]);
-    }
-  }
   const chars = str.split("");
   let stack = [];
-  // Проходимся по строке, проверяя каждый ее символ (п.4).
+
   for (let i = 0; i < chars.length; i += 1) {
-    let openIndex = open.indexOf(chars[i]);
-    if (openIndex !== -1) {
-      // Нашли открывающую скобку. Помещаем ее в стек (п.2).
-      stack.push(chars[i]);
+    let total = brackets.reduce((acc, item) => {
+      if (chars[i] === item) {
+        acc += 1;
+      }
+      acc += 0;
+      return acc;
+    }, 0);
+    if (total === 2) {
+      if (stack.indexOf(chars[i]) === -1) {
+        stack.push(chars[i]);
+      } else {
+        if (stack.indexOf(chars[i]) !== stack.length - 1) {
+          return false;
+        }
+        stack.splice(stack.indexOf(chars[i]), 1);
+      }
       continue;
     }
-    let closeIndex = close.indexOf(chars[i]);
-    if (closeIndex === openIndex) {
-      openIndex = stack.pop();
+    let index = brackets.indexOf(chars[i]);
+    if (index % 2 === 0) {
+      stack.push(chars[i]);
+      continue;
     } else {
-      return false;
+      if (index === brackets.indexOf(stack[stack.length - 1]) + 1) {
+        stack.pop();
+      } else {
+        return false;
+      }
     }
   }
 
   console.log(stack);
-  // Проверяем дисбаланс открытых/закрытых скобок (п.5).
+
   if (stack.length !== 0) {
     return false;
   }
 
   return true;
 }
+
+console.log(
+  check("|(|)", [
+    ["(", ")"],
+    ["|", "|"],
+  ])
+);
