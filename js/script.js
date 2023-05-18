0a265e16df9e6d98ea3954bf3479b81da368c76b
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-05-18 14:31:33 +0300

    add task

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
-const j = "драгоценности";
-const s = "камни";
-
-let arrayJ = j.split("");
-let arrayS = s.split("");
-let total = 0;
-
-for (let i = 0; i < arrayS.length; i += 1) {
-  if (arrayJ.includes(arrayS[i])) {
-    total += 1;
-  } else {
-    total += 0;
+function absentNumber(array) {
+  if (array.length === 1) {
+    return array[0] === 1 ? 2 : 1;
   }
+
+  if (array.length > 0) {
+    const sortedArray = [...array].sort((a, b) => a - b);
+
+    if (sortedArray[0] !== 1) return 1;
+    for (let i = 1; i < sortedArray.length; i += 1) {
+      if (sortedArray[i] !== sortedArray[i - 1] + 1) {
+        return sortedArray[i - 1] + 1;
+      }
+    }
+    return sortedArray[sortedArray.length - 1] + 1;
+  }
+  return 1;
 }
 
-console.log(total);
+console.log(absentNumber([]));
+console.log(absentNumber([1]));
+console.log(absentNumber([2]));
+console.log(absentNumber([2, 3, 4, 5]));
+console.log(absentNumber([1, 2, 3, 4]));
+console.log(absentNumber([1, 2, 4, 5]));
